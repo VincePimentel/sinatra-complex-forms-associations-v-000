3375b8db1e6283b0833e02becdc8243e98e19258
Vince Andrew Pimentel <contact@vincep.me>
2019-01-13 03:22:24 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,53 +2,42 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    
-    erb :'/pets/index'
+    erb :'/pets/index' 
   end
 
-  get '/pets/new' do
-    @owners = Owner.all
-
+  get '/pets/new' do 
     erb :'/pets/new'
   end
 
-  post '/pets' do
+  post '/pets' do 
     @pet = Pet.create(params[:pet])
 
-    if !params[:owner][:name].empty?
-      owner = Owner.create(name: params[:owner][:name])
-      owner.pets << @pet
+    if !params["owner"]["name"].empty?
+    @pet.owner = Owner.create(name: params["owner"]["name"])
     end
-
-    redirect "/pets/#{@pet.id}"
+    @pet.save
+    redirect to "pets/#{@pet.id}"
   end
 
-  get "/pets/:id/edit" do
+   get '/pets/:id' do
     @pet = Pet.find(params[:id])
-    @owners = Owner.all
-
-    erb :"/pets/edit"
-  end
-
-  get '/pets/:id' do
-    @pet = Pet.find(params[:id])
-
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do
-    if !params[:pet].keys.include?(:owner_id)
-      params[:pet][:owner_id] = []
-    end
-
+  patch '/pets/:id' do 
     @pet = Pet.find(params[:id])
-    @pet.update(params[:pet])
 
-    if !params[:owner][:name].empty?
-      owner = Owner.create(name: params[:owner][:name])
-      owner.pets << @pet
+    @pet.update(params["pet"])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
     end
+    @pet.save
 
-    redirect "/pets/#{@pet.id}"
+    redirect "pets/#{@pet.id}"
+  end
+
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
   end
 end
