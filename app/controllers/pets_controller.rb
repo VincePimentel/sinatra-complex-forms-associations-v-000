c7e85a8545281612e250c16c21ea10a74a86134a
Vince Andrew Pimentel <contact@vincep.me>
2019-01-13 03:42:13 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -34,6 +34,7 @@ class PetsController < ApplicationController
     if !params[:owner][:name].empty?
       owner = Owner.create(name: params[:owner][:name])
       owner.pets << @pet
+      owner.save
     end
 
     redirect "/pets/#{@pet.id}"
